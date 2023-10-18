081760b3ea36e40aee240a6a7758fa9f04676a06
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-10-18 21:03:02 +0300

    Finished Header (+ downgraded ANT version)

---
+++ client/src/components/header/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './index.module.css'
+import {Layout, Space, Typography} from 'antd';
+import {LoginOutlined, TeamOutlined, UserAddOutlined} from '@ant-design/icons';
+import {CustomButton} from '../CustomButton';
+import {Link} from 'react-router-dom';
+import {Paths} from '../../paths';
+
+export const Header = () => {
+    return (
+        <Layout.Header className={styles.header}>
+            <Space>
+                <TeamOutlined className={styles.teamIcon}/>
+                <Link to={Paths.home}>
+                    <CustomButton type="ghost">
+                        <Typography.Title level={1}>
+                            Сотрудники
+                        </Typography.Title>
+                    </CustomButton>
+                </Link>
+            </Space>
+            <Space>
+                <Link to={Paths.register}>
+                    <CustomButton type="ghost" icon={<UserAddOutlined/>}>Зарегистрироваться</CustomButton>
+                </Link>
+                <Link to={Paths.login}>
+                    <CustomButton type="ghost" icon={<LoginOutlined/>}>Войти</CustomButton>
+                </Link>
+            </Space>
+        </Layout.Header>
+    );
+};
+
---
+++ client/src/components/layout/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Layout as AntLayout} from "antd";
+import styles from './index.module.css'
+import {Header} from "../header";
+
+type LayoutPropsType = {
+    children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutPropsType> = ({children}) => {
+    return (
+        <div className={styles.main}>
+            <Header/>
+            <AntLayout style = {{height:"100%"}}>
+                {children}
+            </AntLayout>
+        </div>
+    );
+};
+
+export default Layout;
